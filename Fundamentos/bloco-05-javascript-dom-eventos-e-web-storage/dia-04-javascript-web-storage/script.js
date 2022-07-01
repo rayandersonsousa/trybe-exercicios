e57b04bc6e6c7e69c35c1f484bf2e31c486c3a49
Rayanderson Sousa <rayandersoncooper@gmail.com>
2022-07-01 19:20:46 -0300

    Adicionado a opção de mudar o tamanho da fonte

--- Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -3,5 +3,23 @@ window.onload = function() {
   select.addEventListener('change', function(){
     let selected = select.selectedOptions[0];
     document.body.style.backgroundColor = select.value;
+
+    localStorage.setItem('background-color', selected.value);
   })
+
+  let newFontSize = document.querySelector('input[name = "font-size"]');
+  newFontSize.addEventListener('change', function(){
+    let paragrafo = document.querySelector('p');
+    paragrafo.style.fontSize = `${newFontSize.value}px`;
+
+    localStorage.setItem('font-size', `${newFontSize.value}px`);
+  })
+
+
+  let newBackground = localStorage.getItem('background-color');
+  document.body.style.backgroundColor = newBackground;
+
+  let userFontSize = localStorage.getItem('font-size');
+  let paragrafo = document.querySelector('p');
+  paragrafo.style.fontSize = userFontSize;
 }
